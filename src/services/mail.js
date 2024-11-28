f6071a3b5120853d3eb45d28f0b065bf74345bbe
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2024-11-28 19:31:48 +0500

    a

--- src/services/mail.js
+++ src/services/mail.js
@@ -7,23 +7,22 @@ const transport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.MAILTRAP_USERNAME,
-        pass: process.env.MAILTRAP_PASSWORD
-    }
+        pass: process.env.MAILTRAP_PASSWORD,
+    },
 })
 
-export const sendMail = (to, subject, text) =>{
+export const sendMail = (to, subject, text) => {
     transport.sendMail({
         from: process.env.MAILTRAP_USERNAME,
         to,
         subject,
-        text
+        text,
     }),
-    function(error, info){
-        if(error){
-            logger.error(error.massege)
-        }else{
-            logger.info(`sent: ${info.response}`)
+        function (error, info) {
+            if (error) {
+                logger.error(error.massege)
+            } else {
+                logger.info(`sent: ${info.response}`)
+            }
         }
-    }
 }
-
