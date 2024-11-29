6d7bd5ae109b11f3b5a74af90e13e62990532b68
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2024-11-29 17:37:13 +0500

    ayrim kamchilik lar togrilandi

--- src/database/index.js
+++ src/database/index.js
@@ -1,13 +1,7 @@
 import knex from 'knex'
-import { config } from '../configs/index.js'
+import config  from '../../knexfile.js'
 
 export const db = knex({
     client: 'pg',
-    connection: {
-        host: config.postgres.host,
-        port: config.postgres.port,
-        user: config.postgres.user,
-        password: config.postgres.password,
-        database: config.postgres.database,
-    },
+    connection: config.development.connection,
 })
