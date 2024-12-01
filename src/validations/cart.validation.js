54bf348d240f50cf517266e1caa24a6013938126
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2024-12-01 22:35:03 +0500

    validation

--- src/validations/cart.validation.js
+++ src/validations/cart.validation.js
@@ -14,9 +14,7 @@ export const cartSchema = z.object({
         .number()
         .int('Quantity must be an integer') // Butun son
         .positive('Quantity must be greater than zero'), // Musbat son
-    added_at: z
-        .string()
-        .refine((val) => !isNaN(Date.parse(val)), {
-            message: "Invalid date format for 'added_at'",
-        }), // To'g'ri sana formati
+    added_at: z.string().refine((val) => !isNaN(Date.parse(val)), {
+        message: "Invalid date format for 'added_at'",
+    }), // To'g'ri sana formati
 })
