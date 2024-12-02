47589a944f457bdc4de0bb1919f6973e92b7a683
temurparaxatov <temurparaxatov07@gmail.com>
2024-12-02 17:32:49 +0500

    roles

--- migrations/20241126194818_create_users_table.js
+++ migrations/20241126194818_create_users_table.js
@@ -9,7 +9,9 @@ export const up = async (knex) => {
         table.string('username').unique().notNullable()
         table.string('password')
         table.string('google_id')
-        table.enum('role', ['user', 'admin', 'manager','superAdmin']).defaultTo('user')
+        table
+            .enum('role', ['user', 'admin', 'manager', 'superAdmin'])
+            .defaultTo('user')
         table
             .enum('status', ['active', 'inactive', 'banned', 'pending'])
             .defaultTo('inactive')
