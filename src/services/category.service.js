21299b4d0d16f347414a41378a02f285fd7723d7
Xorazm92 <rayadunia2021@gmail.com>
2024-12-02 15:15:39 +0500

    .

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -4,6 +4,7 @@ import { AppError } from '../utils/index.js'
 export const getCategorySevice = async (type, data = '') => {
     try {
         let result
+         
 
         switch (type) {
             case 'all':
