651f43073c18f6f7973f9b3d48b6ff0f72ed9172
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2024-12-04 09:47:59 +0500

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+export const apps = [{
+  script: 'server.js',
+  watch: '.'
+}, {
+  script: './service-worker/',
+  watch: ['./service-worker']
+}];
+export const deploy = {
+  production: {
+    user: 'bitnami',
+    host: '65.2.140.236',
+    ref: 'origin/main',
+    repo: 'https://github.com/MUHAMMADAZIZ008/product-catalog-api',
+    path: 'DESTINATION_PATH',
+    'pre-deploy-local': '',
+    'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+    'pre-setup': ''
+  }
+};
